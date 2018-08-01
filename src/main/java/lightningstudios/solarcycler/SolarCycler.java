7094182faf6a71959f9ad8e8ddb6323e2da75709
lightningstudios126 <ethan.wong@hotmail.com>
2018-08-01 14:10:30 -0400

    bumped version number up a minor release

--- src/main/java/lightningstudios/solarcycler/SolarCycler.java
+++ src/main/java/lightningstudios/solarcycler/SolarCycler.java
@@ -17,7 +17,7 @@ import net.minecraftforge.fml.common.network.NetworkRegistry;
 public class SolarCycler {
     public static final String MODID = "solarcycler";
     public static final String MODNAME = "Solar Cycler";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.1";
     
     @SidedProxy(clientSide = "lightningstudios.solarcycler.proxy.ClientProxy", serverSide = "lightningstudios.solarcycler.proxy.CommonProxy")
     public static CommonProxy proxy;
