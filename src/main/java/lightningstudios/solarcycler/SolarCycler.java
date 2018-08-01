8260904178385cef4b0fec4bd425066a878ff633
lightningstudios126 <ethan.wong@hotmail.com>
2018-07-31 23:33:54 -0400

    minor adjustments

--- src/main/java/lightningstudios/solarcycler/SolarCycler.java
+++ src/main/java/lightningstudios/solarcycler/SolarCycler.java
@@ -17,7 +17,7 @@ import net.minecraftforge.fml.common.network.NetworkRegistry;
 public class SolarCycler {
     public static final String MODID = "solarcycler";
     public static final String MODNAME = "Solar Cycler";
-    public static final String VERSION = "0.0.1";
+    public static final String VERSION = "1.0";
     
     @SidedProxy(clientSide = "lightningstudios.solarcycler.proxy.ClientProxy", serverSide = "lightningstudios.solarcycler.proxy.CommonProxy")
     public static CommonProxy proxy;
